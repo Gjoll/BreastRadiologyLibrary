45092a1808a8b973e9e739abf449d46864e657ce
ROBIN\Kurt <KurtA@Gjoll.com>
2020-03-14 18:26:08 -0500

    .

---
+++ Projects/FireFragger/FireFragger/CS/BuildMembers/BuildMemberExtension.cs
@@ -0,0 +1,87 @@
+﻿using FhirKhit.Tools;
+using FhirKhit.Tools.R4;
+using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace FireFragger.CS.BuildMembers
+{
+    /// <summary>
+    /// </summary>
+    internal abstract class BuildMemberExtension
+    {
+        protected Builder csBuilder => this.defineBase.CSBuilder;
+        protected DefineBase defineBase;
+        protected ClassCodeBlocks codeBlocks;
+        protected ElementTreeNode memberNode;
+
+        public BuildMemberExtension(DefineBase defineBase,
+            ClassCodeBlocks codeBlocks,
+            ElementTreeNode extensionNode)
+        {
+            this.defineBase = defineBase;
+            this.codeBlocks = codeBlocks;
+            this.memberNode = extensionNode;
+        }
+
+        void BuildOneComplexExtension()
+        {
+        }
+
+        void BuildOneSimpleExtension(ElementTreeNode valueXNode)
+        {
+            BuildMemberExtensionSimple bm = new BuildMemberExtensionSimple(this.defineBase,
+                this.codeBlocks,
+                valueXNode);
+            bm.Build();
+        }
+
+        public void BuildOne(ElementTreeSlice extensionSlice)
+        {
+            //$ TODO: Implement validation
+            const String fcn = "BuildSlice";
+
+            if (extensionSlice.Nodes.TryGetItem("value[x]", out ElementTreeNode valueXNode) == false)
+            {
+                this.csBuilder.ConversionError(this.GetType().Name,
+                       fcn,
+                       $"Unimplemented code. value[x] not found");
+                return;
+            }
+
+            if (extensionSlice.Nodes.TryGetItem("extension", out ElementTreeNode subExtensionNode) == false)
+                throw new Exception($"extension.extension is missing");
+            Int32 valueXCardMax = CSMisc.ToMax(valueXNode.ElementDefinition.Max);
+
+            if ((valueXCardMax != 0) && (subExtensionNode.Slices.Count > 1))
+            {
+                // Cant build both simple and complex extension.
+                throw new Exception($"Both Simple and Complex extension found. Not implemented");
+            }
+            else if ((valueXCardMax == 0) && (subExtensionNode.Slices.Count == 0))
+            {
+                // Neither simple and complex extension?
+                throw new Exception($"Neither Simple and Complex extension found. Not implemented");
+            }
+            else if (valueXCardMax != 0)
+            {
+                // Build Simple Extension
+                BuildOneSimpleExtension(valueXNode);
+            }
+            else
+            {
+                // Build Complex Extension
+                BuildOneComplexExtension();
+            }
+        }
+
+        public void Build()
+        {
+            foreach (ElementTreeSlice memberSlice in this.memberNode.Slices.Skip(1))
+                BuildOne(memberSlice);
+        }
+    }
+}
---
+++ Projects/FireFragger/FireFragger/CS/BuildMembers/BuildMemberExtensionSimple.cs
@@ -0,0 +1,103 @@
+﻿using FhirKhit.Tools;
+using FhirKhit.Tools.R4;
+using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FireFragger.CS.BuildMembers
+{
+    internal class BuildMemberExtensionSimple : BuildMemberBase
+    {
+        String itemElementGetName;
+        List<String> itemElementSetName = new List<String>();
+        String containerClassName;
+        protected ElementTreeNode valueNode;
+
+        protected string ElementName => $"{this.valueNode.ElementDefinition.Path.LastPathPart()}";
+        protected override string PropertyName => $"{ElementName.ToMachineName()}";
+        protected override string ElementGetName => this.itemElementGetName;
+        protected override IEnumerable<string> ElementSetNames => this.itemElementSetName;
+        protected override string ContainerClassName => this.containerClassName;
+
+        /// <summary>
+        /// Name of fhir element (as stored in resource).
+        /// </summary>
+        protected override String FhirClassName => "Observation.ComponentComponent";
+
+        /// <summary>
+        /// Perform local processing of container class.
+        /// </summary>
+        protected override void BuildContainerClassLocal(CodeBlockNested containerConstructorBlock,
+            CodeBlockNested containerPropertiesBlock,
+            CodeBlockNested containerMethodsBlock)
+        {
+            base.BuildContainerClassLocal(containerConstructorBlock,
+                containerPropertiesBlock,
+                containerMethodsBlock);
+        }
+
+        protected override void BuildRead(CodeBlockNested b)
+        {
+            b
+                .BlankLine()
+                .AppendCode($"public void Read(BreastRadiologyDocument doc, IEnumerable<{FhirClassName}> components)")
+                .OpenBrace()
+                //.AppendCode($"List<Item> items = new List<Item>();")
+                //.AppendCode($"foreach (Element element in elements)")
+                //.AppendCode($"    items.Add(new Item(({this.ElementGetName}) element));")
+                //.AppendCode($"this.SetAllItems(items);")
+                .CloseBrace()
+                ;
+
+            //this.readBlock
+            //    .AppendCode($"this.{PropertyName}.Read(this.Doc, items);")
+            //    ;
+        }
+
+        protected override void BuildWrite(CodeBlockNested b)
+        {
+            b
+               .AppendCode($"public IEnumerable<{FhirClassName}> Write(BreastRadiologyDocument doc)")
+               .OpenBrace()
+               //.AppendCode($"foreach (Item item in this.GetAllItems())")
+               //.OpenBrace()
+               //.AppendCode($"{FhirClassName} component = new {FhirClassName}")
+               //.OpenBrace()
+               //.AppendCode($"Value = item.Value,")
+               //.AppendCode($"Code = {componentCodeMethodName}()")
+               //.CloseBrace(";")
+               //.AppendCode($"yield return component;")
+               //.CloseBrace()
+               .CloseBrace()
+               ;
+
+            //this.writeBlock
+            //    .AppendCode($"items.AddRange(this.{PropertyName}.Write(this.Doc));")
+            //    ;
+        }
+
+        public override void Build()
+        {
+            Int32 max = CSMisc.ToMax(this.valueNode.ElementDefinition.Max);
+            Int32 min = this.valueNode.ElementDefinition.Min.Value;
+
+            this.itemElementSetName.Clear();
+            foreach (var type in valueNode.ElementDefinition.Type)
+                itemElementSetName.Add(type.Code);
+
+            this.containerClassName = $"{this.ElementName}Container";
+            this.itemElementGetName = (itemElementSetName.Count == 1) ? valueNode.ElementDefinition.Type[0].Code : "Element";
+            base.BuildOne(valueNode.ElementDefinition.ElementId, min, max);
+        }
+
+        public BuildMemberExtensionSimple(DefineBase defineBase,
+            ClassCodeBlocks codeBlocks,
+            ElementTreeNode valueNode) :
+            base(defineBase, codeBlocks)
+        {
+            this.valueNode = valueNode;
+        }
+    }
+}
